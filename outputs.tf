7ba766407a7373130d3c5a793330e133f1ee9a7e
EricRene <58518322+EricRene@users.noreply.github.com>
2025-02-21 19:13:03 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
+output "vpc_id" {
+  value = google_compute_network.vpc.id
+  description = "The VPC network ID"
+}
 
+output "subnet_id" {
+  value = google_compute_subnetwork.subnet.id
+  description = "The Subnet ID"
+}
+
+output "subnet_range" {
+  value = google_compute_subnetwork.subnet.ip_cidr_range
+  description = "The Subnet IP range"
+}
