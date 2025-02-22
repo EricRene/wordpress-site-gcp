b0b108801386815e75cbae26402c2144a2eb0e5b
EricRene <58518322+EricRene@users.noreply.github.com>
2025-02-21 19:12:21 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,28 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
 
+variable "region" {
+  description = "The region in which to provision resources."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC network."
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet."
+  type        = string
+  default     = "my-subnet"
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR range for the subnet."
+  type        = string
+  default     = "10.10.0.0/20"
+}
